e41a78862fa70e3f3b567d2c72be4c70a9a1682e
omowole daniel <omowoledanielsanmi@gmail.com>
2023-12-05 11:39:16 +0100

    first commit

---
+++ orders.js
@@ -0,0 +1,34 @@
+const Orders = [
+{
+    productName: "Foldable Mini Drone",
+    productNumber: "85631",
+    paymentStatus: "Due",
+    shipping: "pending"
+},
+
+{
+    productName: "Lavender KF102 Drone",
+    productNumber: "35531",
+    paymentStatus: "Refunded",
+    shipping: "Declined"
+},
+
+{
+    productName: "Ruko F11 Pro Drone",
+    productNumber: "49341",
+    paymentStatus: "Due",
+    shipping: "pending"
+},
+{
+    productName: "Drone with Camera Drone",
+    productNumber: "95664",
+    paymentStatus: "paid",
+    shipping: "Delivered"
+},
+{
+    productName: "GPS 4K Drone",
+    productNumber: "12331",
+    paymentStatus: "paid",
+    shipping: "Delivered"
+},
+]
